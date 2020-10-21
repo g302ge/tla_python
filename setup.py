18eca11c12498c606291b78141ebd42825817812
Ioannis Filippidis <jfilippidis@gmail.com>
2020-10-22 01:35:21 +0300

    REL: bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ url = 'https://github.com/johnyf/{name}'.format(name=name)
 VERSION_FILE = '{name}/_version.py'.format(name=name)
 MAJOR = 0
 MINOR = 0
-MICRO = 1
+MICRO = 2
 version = '{major}.{minor}.{micro}'.format(
     major=MAJOR, minor=MINOR, micro=MICRO)
 s = (
