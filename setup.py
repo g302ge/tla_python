60dd6d4d9bbdf12bf428bb928d9059a93d871a4a
Ioannis Filippidis <jfilippidis@gmail.com>
2021-08-17 22:35:43 +0200

    REL: append ` :: Only` to Trove classifier for Python 3
    thus representing the current requirement.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ CLASSIFIERS = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
