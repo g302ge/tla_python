tlaplus/tla

REL: pass parameter `project_urls` to `setuptools.setup`
to define:
- the bug tracker
- documentation URL

Relevant references:
- [PEP 345][1]
- [packaging.python.org][2]
- [`setuptools` documentation][3]

[1] https://www.python.org/dev/peps/pep-0345/#project-url-multiple-use
[2] https://packaging.python.org/guides/distributing-packages-using-setuptools/#project-urls
[3] https://setuptools.readthedocs.io/en/latest/userguide/declarative_config.html#metadata

setup.py
@@ -10,6 +10,10 @@ README = 'README.md'
 with open(README) as f:
     long_description = f.read()
 URL = 'https://github.com/tlaplus/tla_python'
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/tlaplus/tla_python/issues',
+    'Documentation':
+        'https://github.com/tlaplus/tla_python/blob/master/doc.md'}
 VERSION_FILE = '{name}/_version.py'.format(name=PACKAGE_NAME)
 MAJOR = 0
 MINOR = 0
@@ -64,6 +68,7 @@ def run_setup():
         author='Ioannis Filippidis',
         author_email='jfilippidis@gmail.com',
         url=URL,
+        project_urls=PROJECT_URLS,
         license='BSD',
         python_requires=PYTHON_REQUIRES,
         install_requires=INSTALL_REQUIRES,

g302ge/tla_python

setup.py
@@ -10,6 +10,10 @@ README = 'README.md'
 with open(README) as f:
     long_description = f.read()
 URL = 'https://github.com/tlaplus/tla_python'
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/tlaplus/tla_python/issues',
+    'Documentation':
+        'https://github.com/tlaplus/tla_python/blob/main/doc.md'}
 VERSION_FILE = '{name}/_version.py'.format(name=PACKAGE_NAME)
 MAJOR = 0
 MINOR = 0
@@ -64,6 +68,7 @@ def run_setup():
         author='Ioannis Filippidis',
         author_email='jfilippidis@gmail.com',
         url=URL,
+        project_urls=PROJECT_URLS,
         license='BSD',
         python_requires=PYTHON_REQUIRES,
         install_requires=INSTALL_REQUIRES,