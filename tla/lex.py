eaa1748023a0243f15a48f8ae9376e473dca49fc
Ioannis Filippidis <jfilippidis@gmail.com>
2021-09-23 23:50:53 +0200

    REF: extract as module `lexer_test`

--- tla/lex.py
+++ tla/lex.py
@@ -8,7 +8,6 @@
 # https://github.com/tlaplus/tlapm/blob/master/src/alexer.mll
 #
 import logging
-import pprint
 import re
 
 import ply.lex
@@ -636,46 +635,19 @@ def _join_with_newlines(tokens):
     return ''.join(strings)
 
 
-MODULE_FOO = r'''
-comments
----- MODULE Foo ----
-VARIABLE x
-
-\* a one-line comment
-\* a nested \* one-line comment
-
-(* This is a multi-line comment. *)
-(* This is a multi-line
-comment. * (
-*)
-(* A nested
-(* multi-line *)
-comment. *)
-a == 1.0
-b == a + 2
-
-A == x' = x + 1
-
-P == SF_x(A)
-
-THEOREM Thm ==
-    ASSUME x = 1
-    PROVE x + 1 = 2
-PROOF
-<1>1. x = 1
-    OBVIOUS
-<1>2. x + 1 = 1 + 1
-    BY <1>1
-<1>3. 1 + 1 = 2
-    OBVIOUS
-<1> QED
-    BY <1>2, <1>3
-====================
-'''
-
-
 def tokenize(data, omit_preamble=True):
-    """Return `list` of `Token` instances."""
+    """Return `list` of `Token` instances.
+
+    @param data: TLA+ source
+    @type data: `str`
+    @param omit_preamble: if `True`,
+        then parse after skipping the preamble.
+        Otherwise, if a preamble is present,
+        then raise a parsing error.
+    @type omit_preamble: `bool`
+    @return: `list` of tokens
+    @rtype: `list` of `tla.tokens.Token`
+    """
     if omit_preamble:
         data = _omit_preamble(data)
     lextokens = _lex(data)
@@ -690,38 +662,3 @@ def tokenize(data, omit_preamble=True):
             module_name=module_name)
         for token in lextokens]
     return tokens
-
-
-def _test_lexer():
-    """Test lexing and conversions between tokens."""
-    data = MODULE_FOO
-    data = _omit_preamble(data)
-    lextokens = _lex(data)
-    # Token_ instances
-    tokens_ = [
-        _map_to_token_(token)
-        for token in lextokens]
-    pprint.pprint(tokens_)
-    for token_ in tokens_:
-        print(token_)
-        print(str(token_))
-    str_of_tokens_ = [
-        str(token_) for token_ in tokens_]
-    pprint.pprint(str_of_tokens_)
-    # Token instances
-    tokens = [
-        _map_to_token(data, token)
-        for token in lextokens]
-    pprint.pprint(tokens)
-    # join raw strings
-    print(''.join(str_of_tokens_))
-    # join with newlines in between
-    s = _join_with_newlines(tokens)
-    print(s)
-    # check location information
-    for token in tokens:
-        print(token.loc)
-
-
-if __name__ == '__main__':
-    _test_lexer()
