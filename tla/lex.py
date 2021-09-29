a53256dc9b6a148e663b50c6ccb0500f29d7d188
Ioannis Filippidis <jfilippidis@gmail.com>
2021-09-29 03:02:25 +0200

    PEP8: avoid alignments across multiple lines

--- tla/lex.py
+++ tla/lex.py
@@ -86,8 +86,8 @@ PREFIX_OPERATORS = {
     'UNCHANGED',
     'UNION',
     }
-letter   = '[a-zA-Z]'
-numeral  = '[0-9]'
+letter = '[a-zA-Z]'
+numeral = '[0-9]'
 # namechar = letter | numeral | _
 namechar = '[a-zA-Z0-9_]'.format(
     letter=letter, numeral=numeral)
@@ -95,7 +95,7 @@ name = f'({namechar})*{letter}({namechar})*'
 
 
 class Lexer:
-    '''Lexer for the TLA+ specification language.'''
+    """Lexer for the TLA+ specification language."""
 
     states = (
         ('multilinecomment', 'exclusive'),
