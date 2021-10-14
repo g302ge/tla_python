f3f6ef6222044209b4c52e75ec901df1cd270709
Ioannis Filippidis <jfilippidis@gmail.com>
2021-10-14 12:54:37 +0200

    REL: support Python 3.10

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Scientific/Engineering',
     'Topic :: Software Development :: Compilers',
     ]
